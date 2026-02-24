7dd28003a666f4d188b2c9b9ca6c81da79dafb70
kcaakash49 <kcaakash4910@gmail.com>
2026-02-24 17:47:23 +0545

    a bit change in gallery page

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -151,6 +151,7 @@ export default async function GalleryPage() {
                       fill
                       sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                       className="object-cover group-hover:scale-105 transition-transform duration-700"
+                      unoptimized={process.env.NODE_ENV !== "production"}
                     />
                   ) : (
                     <div className="w-full h-full flex items-center justify-center">
